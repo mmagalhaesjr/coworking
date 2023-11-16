54d1612317b76bdad0457270f17f0ad9b6cac760
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-11-16 17:41:43 -0300

    update

--- src/pages/Eventos/styled.js
+++ src/pages/Eventos/styled.js
@@ -127,7 +127,7 @@ section{
     -webkit-backdrop-filter: blur(10px);
     -webkit-background-color: rgba(18, 18, 18, 0.2);
     backdrop-filter: blur(10px);
-    background-color: rgba(18, 18, 18, 0.2);
+    background-color: rgba(0, 0, 0, 0.338);
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); 
 
     display: flex;
