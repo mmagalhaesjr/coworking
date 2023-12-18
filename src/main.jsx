82c9c4c2fee69b5f368526b607e1bd783faa9ff0
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-12-18 11:17:35 -0300

    google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import GlobalStyle from './styles/GlobalStyle.js'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-NG7E0CK0ZP");
+
+
+ReactGA.send({ hitType: "pageview", page: "/", title: "www.temasekjf.com.br" });
 
 
 
