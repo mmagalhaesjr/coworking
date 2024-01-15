119aa6d05290733e35ac5197edc9587f0e715bfd
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-01-15 14:53:52 -0300

    blogLista

---
+++ src/components/BlogsLista/BlogsLista.jsx
@@ -0,0 +1,163 @@
+import foto1 from "../../assets/blog/blog1/foto1.webp"
+import foto2 from "../../assets/blog/blog2/foto1.webp"
+import foto3 from "../../assets/blog/blog3/foto1.webp"
+import foto4 from "../../assets/blog/blog4/blog4-f1.webp"
+import foto5 from "../../assets/blog/blog5/foto1.webp"
+import foto6 from "../../assets/blog/blog6/blog6-f1.webp"
+import foto7 from "../../assets/blog/blog7/blog7-f1.webp"
+import foto8 from "../../assets/blog/blog8/blog8-capa.webp"
+import foto9 from "../../assets/blog/blog9/blog9-f2.webp"
+import foto10 from "../../assets/blog/blog10/blog10-f1.webp"
+import foto11 from "../../assets/blog/blog11/foto1.png"
+import foto12 from "../../assets/blog/blog12/foto1.jpg"
+import foto13 from "../../assets/blog/blog13/foto1.png"
+import foto14 from "../../assets/blog/blog14/1.webp"
+import foto15 from "../../assets/blog/blog15/foto1.png"
+import foto16 from "../../assets/blog/blog16/1.png"
+import foto17 from "../../assets/blog/blog17/foto1.png"
+import foto18 from "../../assets/blog/blog18/1.png"
+import foto19 from "../../assets/blog/blog19/1.png"
+import foto20 from "../../assets/blog/blog20/1.png"
+
+export const arrayBlogs = [
+    {
+        id: 20,
+        // link: '/blog20',
+        img: foto20,
+        titulo: 'É possível receber clientes em um coworking?',
+        texto: 'Confira a seguir se é possível receber um cliente em um espaço de trabalho compartilhado.'
+    },
+    {
+        id: 19,
+        // link: '/blog19',
+        img: foto19,
+        titulo: 'Home office: A importância de encontros presenciais no modelo de trabalho híbrido',
+        texto: 'Entenda os 5 motivos pelos quais a interação face a face é crucial na eficácia do trabalho não presencial.'
+    },
+    {
+        id: 18,
+        // link: '/blog18',
+        img: foto18,
+        titulo: 'Coworking para Empreendedores',
+        texto: 'A seguir, vamos falar sobre empreendedorismo e todas as potencialidades do coworking para seu negócio.'
+    },
+    {
+        id: 17,
+        // link: '/blog17',
+        img: foto17,
+        titulo: 'Os 5 principais serviços de um coworking',
+        texto: 'Neste blog, exploraremos os principais serviços que tornam os espaços de coworking uma escolha inteligente.'
+    },
+    {
+        id: 16,
+        // link: '/blog16',
+        img: foto16,
+        titulo: 'Advocacia em Movimento',
+        texto: 'Como os espaços de coworking transformam a rotina dos escritórios jurídicos.'
+    },
+    {
+        id: 15,
+        // link: '/blog15',
+        img: foto15,
+        titulo: 'Por que locar uma sala em um coworking é uma escolha inteligente?',
+        texto: 'Neste blog, exploraremos uma opção que tem ganhado cada vez mais destaque no mundo profissional: a locação de salas em espaços de coworking.'
+    },
+    {
+        id: 14,
+        // link: '/blog14',
+        img: foto14,
+        titulo: 'Zé das Flores: 35 Anos na Mesma Avenida de Juiz de Fora',
+        texto: 'Mais de Três Décadas de Cores e Aromas na Avenida Central da cidade.'
+    },
+    {
+        id: 13,
+        // link: '/blog13',
+        img: foto13,
+        titulo: '3 Dicas para se tornar um empreendedor de sucesso',
+        texto: 'Vamos explorar três dicas fundamentais para ajudar você a trilhar o caminho do empreendedorismo de forma bem-sucedida.'
+    },
+    {
+        id: 12,
+        // link: '/blog12',
+        img: foto12,
+        titulo: 'Mark Zuckerberg - Como construir o futuro',
+        texto: 'Confira a entrevista com Mark Zuckerberg, co-fundador do Facebook e sua empresa-mãe Meta.'
+    },
+    {
+        id: 11,
+        // link: '/blog11',
+        img: foto11,
+        titulo: 'Construindo uma carreira de sucesso: princípios e comportamentos essenciais',
+        texto: 'Neste blog, vamos explorar alguns princípios básicos que podem ajudar você a alcançar o sucesso profissional que deseja.'
+    },
+    {
+        id: 10,
+        // link: '/blog10',
+        img: foto10,
+        titulo: 'Quanto deveria investir em um espaço de coworking?',
+        texto: 'A pergunta que muitos profissionais se fazem é: "Quanto eu devo gastar em um coworking?"...'
+    },
+    {
+        id: 9,
+        // link: '/blog9',
+        img: foto9,
+        titulo: 'Coworking e Bem-Estar: Como criar um espaço de trabalho saudável',
+        texto: 'Neste blog, exploraremos como criar um espaço de coworking que priorize o bem-estar dos membros e promova...'
+    },
+    {
+        id: 8,
+        // link: '/blog8',
+        img: foto8,
+        titulo: 'Descubra o cenário ideal: Como encontrar o espaço de coworking perfeito',
+        texto: 'Vamos explorar as características que definem os melhores espaços...'
+    },
+    {
+        id: 7,
+        // link: '/blog7',
+        img: foto7,
+        titulo: 'Descubra o poder da comunidade: Como os Coworkings estão transformando a maneira como trabalhamos',
+        texto: 'Neste blog, exploraremos como os coworkings estão revolucionando a maneira como trabalhamos...'
+    },
+    {
+        id: 6,
+        // link: '/blog6',
+        img: foto6,
+        titulo: 'Desbloqueando o Potencial: Estratégias para Maximizar sua Experiência em um Coworking',
+        texto: 'Para tirar o máximo proveito de seu espaço de coworking, é importante adotar uma abordagem estratégica, confira a seguir:...'
+    },
+    {
+        id: 5,
+        // link: '/blog5',
+        img: foto5,
+        titulo: 'Trabalho Flexível: Desvendando os Benefícios do Coworking',
+        texto: 'Descubra neste blog como esses espaços de trabalho compartilhados podem revolucionar...'
+    },
+    {
+        id: 4,
+        // link: '/blog4',
+        img: foto4,
+        titulo: 'É possível ter privacidade em um escritório compartilhado?',
+        texto: 'Confira abaixo algumas maneiras de garantir a privacidade em um espaço de coworking...'
+    },
+    {
+        id: 3,
+        // link: '/blog3',
+        img: foto3,
+        titulo: 'Curiosidades sobre o Coworking',
+        texto: 'O que você ainda não sabe sobre os escritórios compartilhados'
+    },
+    {
+        id: 2,
+        // link: '/blog2',
+        img: foto2,
+        titulo: 'Como economizar para investir?',
+        texto: 'Conheça algumas estratégias que podem aumentar suas oportunidades de negócio...'
+    },
+    {
+        id: 1,
+        // link: '/blog1',
+        img: foto1,
+        titulo: 'Por que o coworking é uma escolha inteligente?',
+        texto: 'O coworking abrange um espaço de trabalho compartilhado por profissionais independentes...'
+    },
+];
